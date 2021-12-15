1d6344d966b636c9c5b2ddfc8b55c4bc9d57d46d
Géraud Silvestri <silvestri.geraud@gmail.com>
2021-12-15 11:29:57 +0100

    Création node js - renvoi d'un animal et de son bruit

--- express-image/src/index.js
+++ express-image/src/index.js
@@ -1,4 +1,26 @@
 var Chance = require('chance');
 var chance = new Chance();
 
-console.log("Bonjour " + chance.name());
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+    res.send(f());
+});
+
+app.listen(3000, function () {
+    console.log('Accepting HTTP requests on port 3000\n');
+});
+
+function f(){
+    var numberOfAnimals = chance.integer({min : 1, max : 5});
+
+    var animals = [];
+    for(var i = 0; i < numberOfAnimals; ++i){
+        animals.push({
+            animal: chance.animal(),
+            bruit: chance.sentence()
+        });
+    }
+    return animals;
+}
