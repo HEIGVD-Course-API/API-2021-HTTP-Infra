3b1991f30487d3ea0b34abea5d57d54a741058fb
Androto9 <71312332+Androto9@users.noreply.github.com>
2022-01-12 15:03:42 +0100

    step 2b

--- docker-images/express-image/src/index.js
+++ docker-images/express-image/src/index.js
@@ -1,12 +1,25 @@
 var Chance = require('chance');
+var Express = require('express');
 
 var chance = new Chance();
+var app = new Express();
 
-var nbAnimals = chance.integer({min: 1, max: 10});
-var city = chance.city();
+function displayZoo(){
+	var nbAnimals = chance.integer({min: 1, max: 10});
+	var city = chance.city();
 
-console.log("Number of animals in " + city + " zoo : " + nbAnimals);
+	console.log("Number of animals in " + city + " zoo : " + nbAnimals);
 
-for(var i = 0; i < nbAnimals; ++i){
-	console.log(chance.animal({type: 'zoo'}));
+	var animals=[];
+	for(var i = 0; i < nbAnimals; ++i){
+		animals.push({ animal: chance.animal({ type: 'zoo' }) });
+	}
+	console.log(animals);
+	return animals;
 }
+
+app.get('/zoo', function(req, res){ res.send("Welcome to the zoo");});
+
+app.get('/zoo/animals' , function(req, res){ res.send(displayZoo());});
+
+app.listen(3000, function(){ console.log('Accepting HTTP requets on port 3000.');});
